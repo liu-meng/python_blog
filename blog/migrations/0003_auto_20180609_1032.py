63d9122923e55599c08844b65b6a6f84f76a85f9
LiuMeng <liu_meng@suixingpay.com>
2018-06-09 10:43:46 +0800

    second commit

---
+++ blog/migrations/0003_auto_20180609_1032.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.utils.timezone import utc
+import datetime
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_auto_20180608_2013'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='post',
+            name='create_date',
+            field=models.DateTimeField(default=datetime.datetime(2018, 6, 9, 2, 32, 45, 831800, tzinfo=utc)),
+        ),
+    ]
