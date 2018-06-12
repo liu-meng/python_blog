5d11db7953e3d5e18a00237c1169b8c72f4af135
LiuMeng <liu_meng@suixingpay.com>
2018-06-12 16:34:05 +0800

    logout function

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
@@ -15,23 +16,25 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.save()
-            return  redirect('post_detail',pk=post.pk)
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request,'blog/post_edit.html',{'form':form})
+    return render(request, 'blog/post_edit.html', {'form': form})
 
 
-def post_edit(request,pk):
-    post = get_object_or_404(Post,pk=pk)
-    if request.method=="POST":
-        form = PostForm(request.POST,instance=post)
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
@@ -42,18 +45,21 @@ def post_edit(request,pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('create_date')
-    return render(request,'blog/post_draft_list.html',{'posts':posts})
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
-def post_publish(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     post.publish()
-    return redirect('post_degail',pk=pk)
+    return redirect('post_detail', pk=pk)
 
 
-def post_remove(request,pk):
-    post = get_object_or_404(Post,pk=pk)
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
